eeae0d750bb27567c8428087c2cc0e78005fd704
Maxim Ryabinov <2contact.mr@gmail.com>
2024-04-18 15:13:09 -0400

    Created new service for YouTube Data API and implemented it into the home component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+// Imported Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+
+// App Routing
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { VideoComponent } from './components/video/video.component';
+
+// Services
+import { YoutubeApiService } from './services/youtube-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VideoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [YoutubeApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { YoutubeApiService } from 'src/app/services/youtube-api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  videos: any[] = [];
 
+  loading: boolean = false;
+
+  constructor(private youtubeApiService: YoutubeApiService) {}
+
+  ngOnInit(): void {
+    this.searchVideos();
+  }
+
+  searchVideos(): void {
+    this.loading = true;
+    const query = 'dogs'; // Hardcoded search query for now
+    this.youtubeApiService.searchVideos(query).subscribe(
+      (response: any) => {
+        this.videos = response.items;
+        this.loading = false;
+      },
+      (error: any) => {
+        console.error('Error fetching videos:', error);
+        this.loading = false;
+      }
+    );
+  }
 }
